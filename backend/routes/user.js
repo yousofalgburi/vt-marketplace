93fad8515959d2bb6abefec2e4590023dfbdfd4e
gansh <gansh@vt.edu>
2023-12-02 00:00:45 -0500

    Add user update, change name param, edit user pwd

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,28 @@
 import express from 'express'
-import { signin, signup, getCurrentUser, deleteCurrentUser, deleteUser } from '../controllers/user.js'
+import { signin, signup, getCurrentUser, deleteCurrentUser, deleteUser, updateCurrentUser, updateCurrentUserPWD } from '../controllers/user.js'
 import auth from '../middleware/auth.js'
 import { getPostsByUser } from '../controllers/home.js'
 
 const router = express.Router()
 
+//get current user
 router.get('/currentUser', auth, getCurrentUser)
 
+//account creation, signin
 router.post('/signin', signin)
 router.post('/signup', signup)
+
+//acount deletion
 router.delete('/delete', auth, deleteCurrentUser)
 router.delete('/delete/:userID', auth, deleteUser)
 
+//get posts by user
 router.get('/posts/:userID', auth, getPostsByUser)
+
+//update user info: fname, lname, email
+router.patch('/update', auth, updateCurrentUser)
+
+//update password
+router.patch('/updatePassword', auth, updateCurrentUserPWD)
 
 export default router
