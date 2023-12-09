76b1ff0cd36fd4a7eb840a76f9057542910b1c1b
gansh <gansh@vt.edu>
2023-12-08 22:18:04 -0500

    Add user existence check in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,26 @@
 import jwt from 'jsonwebtoken'
+import UserModel from '../models/user.js'; // Import your User model
+
 const secret = process.env.SECERT || 'test'
 
 const auth = async (req, res, next) => {
-	// console.log(req.headers)
-	try {
-		const token = req.headers.authorization
-		let decodedData = jwt.verify(token, secret)
-		req.userId = decodedData?.id
-		if(req.body.userId && req.body.userId !== req.userId) {
-			return res.status(401).json({ message: 'Unauthorized' })
-		}
-		next()
-	} catch (error) {
-		res.status(500).json({ message: error.message })
-	}
+    try {
+        const token = req.headers.authorization
+        let decodedData = jwt.verify(token, secret)
+        req.userId = decodedData?.id
+
+        // Check if user exists in database
+        const userExists = await UserModel.findById(req.userId)
+        if (!userExists) return res.status(401).json({ message: 'Unauthorized' })
+
+        if(req.body.userId && req.body.userId !== req.userId) {
+            return res.status(401).json({ message: 'Unauthorized' })
+        }
+
+        next()
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 export default auth
