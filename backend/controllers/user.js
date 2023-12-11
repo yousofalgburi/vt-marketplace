f006960c8b4205c055c0881fffb05c57ed58b39f
gansh <gansh@vt.edu>
2023-12-11 08:40:05 -0500

    Fix user password visibility

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -90,6 +90,7 @@ export const getCurrentUser = async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
+		user.password = undefined
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
